53ef623e3e96abc60fefc8c085161edfbb53a1ad
sandraoliv <sandra.so219@gmail.com>
2023-06-06 17:32:53 -0300

    fix :choices

--- pets/models.py
+++ pets/models.py
@@ -3,17 +3,21 @@ from groups.models import Group
 from traits.models import Trait
 
 
+class SEX_CHOICES(models.TextChoices):
+    MALE = ("Male",)
+    FEMALE = ("Female",)
+    NOT_INFORMED = "Not Informed"
+
+
 class Pet(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     weight = models.FloatField()
-    SEX_CHOICES = [
-        ("Male", "Male"),
-        ("Female", "Female"),
-        ("Not Informed", "Not Informed"),
-    ]
-    sex = models.CharField(max_length=20, choices=SEX_CHOICES, default="Not Informed")
-    group = models.ForeignKey("groups.Group", on_delete=models.PROTECT, related_name="pets")
+    sex = models.CharField(max_length=20, choices=SEX_CHOICES.choices, default=SEX_CHOICES.NOT_INFORMED)
+       
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.PROTECT, related_name="pets"
+    )
     traits = models.ManyToManyField("traits.Trait", related_name="pets")
 
     def __str__(self):
