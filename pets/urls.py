f134fad0958218d34780b6bae4c3b324c9167bb1
sandraoliv <sandra.so219@gmail.com>
2023-06-11 10:09:10 -0300

    feat:tarefa 3 finalizada

--- pets/urls.py
+++ pets/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import PetView
+from pets import views
+
 
 urlpatterns = [
     path('pets/', PetView.as_view()),
--- pets/views.py
+++ pets/views.py
@@ -1,23 +1,51 @@
 from rest_framework.views import APIView, Request, Response, status
-from .models import Pet
-from .serializers import PetSerializer
-from django.shortcuts import get_object_or_404
+from pets.models import Pet
+from pets.serializer import PetSerializer
+from groups.models import Group
+from traits.models import Trait
+from rest_framework.pagination import PageNumberPagination
 
 
-class PetView(APIView):
+class PetView(APIView, PageNumberPagination):
     def get(self, request: Request) -> Response:
         pets = Pet.objects.all()
 
-        serializer = PetSerializer(instance=pets, many=True)
+        result_page = self.paginate_queryset(pets, request)
 
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+        serializer = PetSerializer(instance=result_page, many=True)
+
+        return self.get_paginated_response(serializer.data)
 
     def post(self, request: Request) -> Response:
         serializer = PetSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+      
+        group_data = serializer.validated_data.pop('group')
 
-        pet = Pet.object.create(**serializer.valideted_data)
+        group, _ = Group.objects.get_or_create(scientific_name=group_data['scientific_name'])
+
+        traits_data = serializer.validated_data.pop('traits')
+
+        pet = Pet.objects.create(group=group, **serializer.validated_data)
+
+        traits = []
+        for trait_data in traits_data:
+            trait, _ = Trait.objects.get_or_create(trait_name__iexact=trait_data['trait_name'])
+            
+            traits.append(trait)
+            pet.traits.add(*traits)
+
         serializer = PetSerializer(instance=pet)
-
+        
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+   
+
+
+
+       
+
+       
+
+      
